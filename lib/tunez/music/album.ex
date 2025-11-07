2d5fa7c9b1525957640766d512169a4dc5caaf5a
Victor Kryukov <victor.kryukov@gmail.com>
2025-11-06 18:11:00 -0800

    Add Album resource

---
+++ lib/tunez/music/album.ex
@@ -0,0 +1,35 @@
+defmodule Tunez.Music.Album do
+  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+
+  postgres do
+    table "albums"
+    repo Tunez.Repo
+
+    references do
+      reference :artist, index?: true
+    end
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string do
+      allow_nil? false
+    end
+
+    attribute :year_released, :integer do
+      allow_nil? false
+    end
+
+    attribute :cover_image_url, :string
+
+    create_timestamp :inserted_at
+    create_timestamp :updated_at
+  end
+
+  relationships do
+    belongs_to :artist, Tunez.Music.Artist do
+      allow_nil? false
+    end
+  end
+end
